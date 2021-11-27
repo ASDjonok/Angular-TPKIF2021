0f4e68b8e790dd0a920d0e12e70013f1bf07de3c
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-11-27 14:08:05 +0200

    add custom model and custom components // add data connection between view and model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: '<h1>Hello World!</h1>',
+  styleUrls: ['./app.component.css']/*,
+  template: `<p>Привет {{name}}</p>
+                <input type="text" [(ngModel)]="name" /> <br><br>
+                <input type="text" [(ngModel)]="name" />`*/
 })
 export class AppComponent {
-  title = 'Angular-TPKIF2021';
+  title = 2;
+  text = "Hello world!";
+  condition = false;
+  items = ["i1", "i2", "i3"];
+  // name = "Tom";
+  colspan = 2;
+  count: number = 0;
+  name: string = "Tom";
+  // initialInputValue: number = 124;
+  initialInputValue: string = "125";
+
+  increase(): void {
+    this.count++;
+  }
+
+  increase2($event: any): void {
+    this.count++;
+    console.log($event);
+  }
+
+  toggle() {
+    console.log("in toggle!");
+    this.text = "Bye world!";
+    this.condition = !this.condition;
+    let a/*:String*/;
+    a = "ASD";
+    console.log("a", a);
+    a = 2;
+    console.log("a", a);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChildCompComponent } from './child-comp/child-comp.component';
+import {MyModuleModule} from "./my-module/my-module.module";
+import { SecondChildComponentComponent } from './second-child-component/second-child-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildCompComponent,
+    SecondChildComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyModuleModule,
+    // MyModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
